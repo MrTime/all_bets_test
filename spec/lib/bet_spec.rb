fa7e161e73d4589c9825461632fd55d3fa685908
fedir.kovbel <kfedia@gmail.com>
2013-04-03 15:23:05 +0300

    refactored tests

--- spec/lib/bet_spec.rb
+++ spec/lib/bet_spec.rb
@@ -6,13 +6,15 @@ describe Bet do
   let(:parser) { Parser.new :sport_id => 11 }
   let(:doc) { Nokogiri::HTML(open('spec/support/bets.html')) }
 
-  it 'should understand values' do
-    Bet.parse(doc) do |result|
-      res = { :event_id => '805371', 
+  BET_VALUES = { :event_id => '805371', 
               :category => 'Match_Result', 
               :bet => '1'}
-      result.should eq res
+
+  BET_VALUES.each_pair do |key,value|
+    it "should parse #{key.to_s}" do
+      Bet.parse(doc) do |result|
+        result[key].should eq value
+      end
     end
-    
   end
 end

036ecc45f789028c795d15e9c6efb73792fd9d9c
fedir.kovbel <kfedia@gmail.com>
2013-04-03 17:28:05 +0300

    adding parsing koeffs & values of bets

--- lib/bet.rb
+++ lib/bet.rb
@@ -3,9 +3,16 @@ class Bet
     doc.css('.selection-price').each_with_index do |link,i|
       if i%2 == 0
         params = /(\d+)@(\w*).(.*)/.match(link['id'])
+
+        koeff = link.parent.css('.selection-link').inner_text.to_f
+        value = /\((.+)\).*/.match(link.parent.content.strip)
+        value = value.nil? ? nil : value[1]
+
         options = {
           :event_id => params[1],
+          :koeff => koeff,
           :category => params[2],
+          :value => value,
           :bet => params[3]
         }
         yield options
--- spec/lib/bet_spec.rb
+++ spec/lib/bet_spec.rb
@@ -8,13 +8,38 @@ describe Bet do
 
   BET_VALUES = { :event_id => '805371', 
               :category => 'Match_Result', 
+              :koeff => 2.0,
               :bet => '1'}
+
+  it 'should parse all bets' do
+    results = []
+    Bet.parse(doc) do |result|
+      results << result;
+    end
+
+    results.size.should eq 4
+  end
 
   BET_VALUES.each_pair do |key,value|
     it "should parse #{key.to_s}" do
+      results = []
       Bet.parse(doc) do |result|
-        result[key].should eq value
+        results << result;
       end
+
+      results[0][key].should eq value
     end
+  end
+
+  it 'should parse value of bet' do
+    results = []
+    Bet.parse(doc) do |result|
+      results << result;
+    end
+
+    results[0][:value].should be_nil
+    results[1][:value].should eq '-1'
+    results[2][:value].should eq '+1'
+    results[3][:value].should eq '2.5'
   end
 end

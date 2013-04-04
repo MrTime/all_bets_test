e088073725e631a4c93b6f23226c7f0b6533db33
fedir.kovbel <kfedia@gmail.com>
2013-04-04 14:13:27 +0300

    refactored tests

--- spec/lib/bet_spec.rb
+++ spec/lib/bet_spec.rb
@@ -5,6 +5,13 @@ describe Bet do
 
   let(:parser) { Parser.new :sport_id => 11 }
   let(:doc) { Nokogiri::HTML(open('spec/support/bets.html')) }
+  let(:results) do
+    results = []
+    Bet.parse(doc) do |result|
+      results << result;
+    end
+    results
+  end
 
   BET_VALUES = { :event_id => '805371', 
               :category => 'Match_Result.1', 
@@ -12,31 +19,16 @@ describe Bet do
               :rel => 'rel'}
 
   it 'should parse all bets' do
-    results = []
-    Bet.parse(doc) do |result|
-      results << result;
-    end
-
     results.size.should eq 6
   end
 
   BET_VALUES.each_pair do |key,value|
     it "should parse #{key.to_s}" do
-      results = []
-      Bet.parse(doc) do |result|
-        results << result;
-      end
-
       results[0][key].should eq value
     end
   end
 
   it 'should parse value of bet' do
-    results = []
-    Bet.parse(doc) do |result|
-      results << result;
-    end
-
     results[0][:value].should be_nil
     results[1][:value].should eq '-1'
     results[2][:value].should eq '+1'
@@ -44,13 +36,12 @@ describe Bet do
   end
 
   it 'should parse period of bet' do
-    results = []
-    Bet.parse(doc) do |result|
-      results << result;
-    end
-
     results[0][:period].should eq 'Full_Match'
     results[4][:period].should eq '1st_Half'
     results[5][:period].should eq '2nd_Half'
+  end
+
+  it 'should parse event home' do
+
   end
 end
